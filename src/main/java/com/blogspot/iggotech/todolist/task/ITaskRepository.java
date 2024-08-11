1b94f05f8a89f94f79ebfc34271edc1121b84f10
iggonicollas <iggonicollas@gmail.com>
2024-08-11 10:27:33 -0300

    password check acceptance

---
+++ src/main/java/com/blogspot/iggotech/todolist/task/ITaskRepository.java
@@ -0,0 +1,10 @@
+package com.blogspot.iggotech.todolist.task;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ITaskRepository extends JpaRepository <TaskModel, UUID> {
+
+
+}
